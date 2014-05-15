5425ada8dc588d0674eda51d44153f7467a7663f
fuxes <mrtnbcrr@gmail.com>
2014-05-15 12:33:57 -0300

    First try with 2ops. Needs to be checked

--- 2op_algorithm.py
+++ 2op_algorithm.py
@@ -1,18 +1,20 @@
 import random
 import math
 
+inf = float("inf")
 # ipython notebook + matplotlib (o gnuplot)
 def experimento(N, S, Tf, Tr):
     t = 0
     r = 0
-    t_ = float("inf")
+    t1_ = inf
+    t2_ = inf
      
     exponencial = (lambda x: -math.log(random.random()) * x)
     events = [exponencial(Tf) for _ in range(N)]
     events.sort()
 
     while True:
-        if (events[0] < t_): # Caso 1: Falla una maquina (mqn)
+        if (events[0] < t1_ and events[0] < t2_): # Caso 1: Falla una maquina (mqn)
             t = events[0]
             r += 1
             if (r == S+1): # Mqs rotas > Repuestos
@@ -21,16 +23,27 @@ def experimento(N, S, Tf, Tr):
                 x = exponencial(Tf)
                 events[0] = t + x # tiempo en el q se rompe el repuesto
                 events.sort()
-                if (t_ == float("inf")): # op idle == r=1
-                    t_ = t + exponencial(Tr)
+                if (t1_ == inf): # op1 idle
+                    t1_ = t + exponencial(Tr)
+                    continue
+                if (t2_ == inf): # op2 idle
+                    t2_ = t + exponencial(Tr)
 
-        else: # Caso 2: Se repara una mqn
-            t = t_
+        elif (events[0] >= t1_): # op1 ends
+            t = t1_
             r -= 1
-            if (r > 0): # Si hay, arreglar sig mqn
-                t_ = t + exponencial(Tr)
+            if ((r > 1 and t2_ != inf) or (t2_ == inf and r > 0)):
+                t1_ = t + exponencial(Tr)
             else: # Si no, operario inactivo
-                t_ = float("inf")
+                t1_ = inf
+
+        elif (events[0] >= t2_): # op2 ends
+            t = t2_
+            r -= 1
+            if ((r > 1 and t1_ != inf) or (t1_ == inf and r > 0)):
+                t2_ = t + exponencial(Tr)
+            else: # Si no, operario inactivo
+                t2_ = inf
 
 if __name__ == "__main__":
     for N in [100, 1000, 10000]:
