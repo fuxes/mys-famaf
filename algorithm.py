3d1c1339107d02481914b75046a43750c90175ec
fuxes <mrtnbcrr@gmail.com>
2014-05-15 11:55:50 -0300

    esperanza working

--- algorithm.py
+++ algorithm.py
@@ -5,49 +5,44 @@ import math
 
 def exponencial(l):
     u = random.random() 
-    t = float(((-1.0) / float(l)) * math.log(u))
-    return t
-
-def eventos(n, l):
-    t = []
-    for _ in range(n):
-        x = float(exponencial(l))
-        t.append(x)
+    #t = - math.log(u) / float(l)
+    t = - math.log(1-random.random()) * l    
     return t
 
 def experimento(N, S, Tf, Tr):
     t = 0
     r = 0
     t_ = float("inf")
-    
-    xs = eventos(N, Tf) # Genera la lista de eventos
-    xs.append(t_)
-    xs.sort()
+     
+    exponencial = (lambda x: -math.log(random.random()) * x)
+    events = [exponencial(Tf) for _ in range(N)]
+    events.sort()
 
     while True:
-        if (xs[0] < t_): # Caso 1: Falla una maquina (mqn)
-            t = xs[0]
+        if (events[0] < t_): # Caso 1: Falla una maquina (mqn)
+            t = events[0]
             r += 1
-            if (r == S+1): # Mqs > Repuestos
+            if (r == S+1): # Mqs rotas > Repuestos
                 return t;
             else:
                 x = exponencial(Tf)
-                xs[0] = t + x # Se vuelve a romper en t+x
-                xs.sort()
+                events[0] = t + x # tiempo en el q se rompe el repuesto
+                events.sort()
                 if (r == 1): # operario emp a trabajar
-                    t_ = float(t + exponencial(1 / float(Tr)))
+                    t_ = t + exponencial(Tr)
 
         else: # Caso 2: Se repara una mqn
             t = t_
             r -= 1
             if (r > 0): # Si hay, arreglar sig mqn
-                t_ = float(t + exponencial(1/float(Tr)))
+                t_ = t + exponencial(Tr)
             else: # Si no, operario inactivo
                 t_ = float("inf")
 
 if __name__ == "__main__":
-    t = 0
     for N in [100, 1000, 10000]:
+        t = 0
         for _ in range(N):
             t += experimento(5, 2, 1, 0.125)
-        print t / float(N)
+        e = t / float(N)
+        print e
